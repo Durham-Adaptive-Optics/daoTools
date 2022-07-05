cd78105ae1c882ae6f79d74c7a28152a8b3a999b
scetre <scetre@protonmail.com>
2022-07-05 12:21:30 +0100

    wip

--- apps/daoCommandFilter.c
+++ apps/daoCommandFilter.c
@@ -112,7 +112,7 @@ static int realTimeLoop()
         clock_gettime(CLOCK_REALTIME, &t[1]);
         elapsedTime = (t[1].tv_sec - t[0].tv_sec) * 1e3;
         elapsedTime += (t[1].tv_nsec - t[0].tv_nsec) / 1e6;
-        printf("\r fps = %.3f Hz, %d in=[%6.3f,%6.3f,...,%6.3f], out[%6.3f, %6.3f,...,%6.3f]", 1e6/(1000*elapsedTime), 
+        printf("\r fps = %8.3f Hz, %d in=[%6.3f,%6.3f,...,%6.3f], out[%6.3f, %6.3f,...,%6.3f]", 1e6/(1000*elapsedTime), 
                                                                               inSize, inShm[0].array.F[0],
                                                                               inShm[0].array.F[1],
                                                                               inShm[0].array.F[inSize],
