b472420089dec92591b2dd22a44adb49253b5fe3
wivern <wivern@cockatrice.dragon.local>
2022-11-08 15:41:39 +0000

    Added axis flip option

--- apps/daoImageRTD.py
+++ apps/daoImageRTD.py
@@ -9,10 +9,11 @@ import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     shmimName = 'WFSim.im.sh'
+    axisFlip = False 
     try:
-        opts, args = getopt.getopt(sys.argv[1:],"hs:",["help", "shmimName="])
+        opts, args = getopt.getopt(sys.argv[1:],"hs:a",["help", "shmimName=", "axisFlip"])
     except getopt.GetoptError:
-      print('err, usage: doaImageRTD.py -s <shmimName>')
+      print('err, usage: doaImageRTD.py -s <shmimName> -a')
       sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
@@ -20,17 +21,23 @@ if __name__ == '__main__':
             sys.exit()
         elif opt in ("-s", "--shm"):
             shmimName = str(arg)
+        elif opt in ("-a", "--axisFlip"):
+            axisFlip = True
     shm = shmlib.shm(shmimName)
     imageSizeX = shm.get_data().shape[0]
     imageSizeY = shm.get_data().shape[1]
+    if axisFlip == True:
+        imageSizeX = shm.get_data().shape[1]
+        imageSizeY = shm.get_data().shape[0]
+
     plt.ion()
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    a=plt.imshow(shm.get_data().flatten().reshape((imageSizeY,imageSizeX)))
+    a=plt.imshow(shm.get_data().flatten().reshape((imageSizeX,imageSizeY)))
     plt.colorbar()
     plt.show()
     while True:
-        a.set_data(shm.get_data().flatten().reshape((imageSizeY,imageSizeX)))
+        a.set_data(shm.get_data().flatten().reshape((imageSizeX,imageSizeY)))
         fig.canvas.draw()
         fig.canvas.flush_events()
-        time.sleep(0.05)
+        time.sleep(0.01)
