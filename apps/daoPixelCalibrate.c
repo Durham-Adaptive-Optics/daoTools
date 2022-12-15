d4cb0c239377c38da959e63c4696fb230abbacb1
scetre <scetre@protonmail.com>
2022-12-15 10:59:47 +0000

    Creation

---
+++ apps/daoPixelCalibrate.c
@@ -0,0 +1,296 @@
+/*****************************************************************************
+  DAO project
+  s.cetre
+ *****************************************************************************/
+
+/*==========================================================================*/
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <math.h>
+#include <semaphore.h>
+#include <sched.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <ctype.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <sys/file.h>
+#include <errno.h>
+#include <sys/mman.h>
+#include <sched.h>
+#include <semaphore.h>
+#include <sys/time.h>
+#include <pthread.h>
+
+#include "daoShm.h"
+
+/*==========================================================================*/
+static int	sNdx=0;							/* board index */
+static int	sExit=0;						/* program exit code */
+
+//Need to install process with setuid.  Then, so you aren't running privileged all the time do this:
+uid_t euid_real;
+uid_t euid_called;
+uid_t suid;
+
+struct timespec tnow;
+double tnowdouble;
+double tlastupdatedouble;
+
+
+char inShmName[32];
+char ffShmName[32];
+char bgShmName[32];
+char calShmName[32];
+
+static int   		end     = 0;		           // termination flag
+// termination function for SIGINT callback
+static void endme()
+{
+    end = 1;
+}
+
+/*--------------------------------------------------------------------------*/
+static char	*sArgv0=NULL;					/* name of executable */
+
+static void ShowHelp(void)
+{
+    daoInfo("%s of " __DATE__ " at " __TIME__ "\n",sArgv0);
+    daoInfo("   arguments:\n");
+    daoInfo("   -h               display this message and exit\n");
+    daoInfo("   -d               display program debug output\n");
+    /*
+     **	Post init tests
+     */
+    daoInfo("   -L sham              real time control loop\n");
+    daoInfo("\n");
+}
+
+/*--------------------------------------------------------------------------*/
+static int realTimeLoop()
+{
+    // register interrupt signal to terminate the main loop
+    signal(SIGINT, endme);
+    IMAGE *inShm;
+    IMAGE *ffShm;
+    IMAGE *bgShm;
+    IMAGE *calShm;
+
+    inShm = (IMAGE*) malloc(sizeof(IMAGE));
+    ffShm = (IMAGE*) malloc(sizeof(IMAGE));
+    bgShm = (IMAGE*) malloc(sizeof(IMAGE));
+    calShm = (IMAGE*) malloc(sizeof(IMAGE));
+    daoShmShm2Img(inShmName, "", &inShm[0]);
+    daoShmShm2Img(ffShmName, "", &ffShm[0]);
+    daoShmShm2Img(bgShmName, "", &bgShm[0]);
+    daoShmShm2Img(calShmName, "", &calShm[0]);
+
+    daoInfo("Starting loop, %s/%s \n",inShmName, calShmName);
+    fflush(stdout);
+    int inSize = inShm[0].md[0].size[0]*inShm[0].md[0].size[1];
+    int calSize = calShm[0].md[0].size[0]*calShm[0].md[0].size[1];
+    struct timespec t[3];
+    struct timespec timeout;
+    double elapsedTime;
+    double calTime;
+    clock_gettime(CLOCK_REALTIME, &t[1]);
+    int k;
+    int waitCounter = 0;
+    while (end ==0)
+    {
+        t[0] = t[1];
+        clock_gettime(CLOCK_REALTIME, &timeout);
+        timeout.tv_sec += 1; // 1 second timeout
+        if (sem_timedwait(inShm[0].semptr[2], &timeout) != -1)
+        {
+            clock_gettime(CLOCK_REALTIME, &t[2]);
+
+            // New image, insert something here
+            calShm[0].md[0].cnt2 = inShm[0].md[0].cnt2;
+            if (inShm[0].md[0].atype == _DATATYPE_UINT8)
+            {
+                for (k = 0; k < calSize; k++)
+                {
+                    calShm[0].array.F[k] = ((float)inShm[0].array.UI8[k] * ffShm[0].array.F[k]) - bgShm[0].array.F[k];
+                }
+            }
+            else if (inShm[0].md[0].atype == _DATATYPE_INT8)
+            {
+                for (k = 0; k < calSize; k++)
+                {
+                    calShm[0].array.F[k] = ((float)inShm[0].array.SI8[k] * ffShm[0].array.F[k]) - bgShm[0].array.F[k];
+                }
+            }
+            else if (inShm[0].md[0].atype == _DATATYPE_UINT16)
+            {
+                for (k = 0; k < calSize; k++)
+                {
+                    calShm[0].array.F[k] = ((float)inShm[0].array.UI16[k] * ffShm[0].array.F[k]) - bgShm[0].array.F[k];
+                }
+            }
+            else if (inShm[0].md[0].atype == _DATATYPE_INT16)
+            {
+                for (k = 0; k < calSize; k++)
+                {
+                    calShm[0].array.F[k] = ((float)inShm[0].array.SI16[k] * ffShm[0].array.F[k]) - bgShm[0].array.F[k];
+                }
+            }
+            else if (inShm[0].md[0].atype == _DATATYPE_INT32)
+            {
+                for (k = 0; k < calSize; k++)
+                {
+                    calShm[0].array.F[k] = ((float)inShm[0].array.UI32[k] * ffShm[0].array.F[k]) - bgShm[0].array.F[k];
+                }
+            }
+            else if (inShm[0].md[0].atype == _DATATYPE_UINT32)
+            {
+                for (k = 0; k < calSize; k++)
+                {
+                    calShm[0].array.F[k] = ((float)inShm[0].array.SI32[k] * ffShm[0].array.F[k]) - bgShm[0].array.F[k];
+                }
+            }
+            else if (inShm[0].md[0].atype == _DATATYPE_UINT64)
+            {
+                for (k = 0; k < calSize; k++)
+                {
+                    calShm[0].array.F[k] = ((float)inShm[0].array.UI64[k] * ffShm[0].array.F[k]) - bgShm[0].array.F[k];
+                }
+            }
+            else if (inShm[0].md[0].atype == _DATATYPE_INT64)
+            {
+                for (k = 0; k < calSize; k++)
+                {
+                    calShm[0].array.F[k] = ((float)inShm[0].array.SI64[k] * ffShm[0].array.F[k]) - bgShm[0].array.F[k];
+                }
+            }
+            else if (inShm[0].md[0].atype == _DATATYPE_FLOAT)
+            {
+                for (k = 0; k < calSize; k++)
+                {
+                    calShm[0].array.F[k] = ((float)inShm[0].array.F[k] * ffShm[0].array.F[k]) - bgShm[0].array.F[k];
+                }
+            }
+            else if (inShm[0].md[0].atype == _DATATYPE_DOUBLE)
+            {
+                for (k = 0; k < calSize; k++)
+                {
+                    calShm[0].array.F[k] = ((float)inShm[0].array.D[k] * ffShm[0].array.F[k]) - bgShm[0].array.F[k];
+                }
+            }
+            daoShmImagePart2ShmFinalize(&inShm[0]);
+            clock_gettime(CLOCK_REALTIME, &t[1]);
+            elapsedTime = (t[1].tv_sec - t[0].tv_sec) * 1e3;
+            elapsedTime += (t[1].tv_nsec - t[0].tv_nsec) / 1e6;
+            calTime = (t[1].tv_sec - t[2].tv_sec) * 1e3;
+            calTime += (t[1].tv_nsec - t[2].tv_nsec) / 1e6;
+            printf("\rcal time = %8.3f us, fps = %8.3f Hz, %d, cal[%6.3f, %6.3f,...,%6.3f]", 
+                   1000*calTime,
+                   1e6 / (1000 * elapsedTime),
+                   inSize, 
+                   calShm[0].array.F[0],
+                   calShm[0].array.F[1],
+                   calShm[0].array.F[calSize]);
+        }
+        else
+        {
+            waitCounter += 1;
+            printf("\rWAIT %d", waitCounter);
+        }
+        fflush(stdout);
+    }
+
+
+    daoInfo("EXITING MAIN LOOP\n");
+    fflush(stdout);
+
+
+
+    return 0;
+}
+
+/**
+ *	Parse the input arguments.
+ */
+static void DecodeArgs(int argc, char **argv)
+{
+    char	*str;
+    int a1;
+
+    argv += 1;	argc -= 1;					/* skip program name */
+
+    while (argc-- > 0) {
+        daoDebug("DecodeArgs: working on '%s'/%d\n",*argv,argc);
+        str = *argv++;
+        if (str[0] != '-') {
+            daoError("Do not know arg '%s'\n",str);
+            ShowHelp();
+            exit(1);
+        }
+
+        switch (str[1]) {
+            case 'h':	ShowHelp(); exit(0);
+            case 'd':	
+                        (void)sscanf(*argv++,"%d",&daoLogLevel); argc -= 1;
+                        break;
+            case 'l':
+                        daoInfo("%s\n",*argv);
+                        argv += 1; argc -= 1;
+                        break;
+
+            case 'b':	(void)sscanf(*argv++,"%d",&sNdx); argc -= 1;	break;
+            case 'u':
+                        (void)sscanf(*argv++,"%d",&a1); argc -= 1;
+                        daoDebug("will sleep for %d usec\n",a1);
+                        (void)usleep(a1);
+                        break;
+                        break;
+            case 'L':
+                        daoInfo("Apply Falt and Background from SHM real time control\n");
+                    	(void)sscanf(*argv++,"%s",inShmName); argc -= 1;
+                    	(void)sscanf(*argv++,"%s",ffShmName); argc -= 1;
+                    	(void)sscanf(*argv++,"%s",bgShmName); argc -= 1;
+                    	(void)sscanf(*argv++,"%s",calShmName); argc -= 1;
+                        daoInfo("image in         : %s\n", inShmName);
+                        daoInfo("flat field       : %s\n", ffShmName);
+                        daoInfo("background       : %s\n", bgShmName);
+                        daoInfo("calibrated image : %s\n", calShmName);
+                        realTimeLoop();
+                        break;
+            default:
+                        daoError("Do not know arg '%s'\n",str);
+                        ShowHelp();
+                        exit(2);
+        }
+    }
+
+    return;
+}
+
+/*==========================================================================*/
+int main(int argc, char **argv)
+    /*
+     **	Fetch the arguments and do what is requested
+     */
+{
+    int RT_priority = 93; //any number from 0-99
+    struct sched_param schedpar;
+
+    schedpar.sched_priority = RT_priority;
+    // r = seteuid(euid_called); //This goes up to maximum privileges
+    sched_setscheduler(0, SCHED_FIFO, &schedpar); //other option is SCHED_RR, might be faster
+    // r = seteuid(euid_real);//Go back to normal privileges
+
+    sArgv0 = *argv;
+
+    DecodeArgs(argc,argv);
+
+    return(sExit);
+}
+/*==========================================================================*/
+
---
+++ apps/daoShmCombiner.c
@@ -0,0 +1,297 @@
+/*****************************************************************************
+  DAO project
+  s.cetre
+ *****************************************************************************/
+
+/*==========================================================================*/
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <math.h>
+#include <semaphore.h>
+#include <sched.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <ctype.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <sys/file.h>
+#include <errno.h>
+#include <sys/mman.h>
+#include <sched.h>
+#include <semaphore.h>
+#include <sys/time.h>
+#include <termios.h>
+#include <pthread.h>
+#include <ncurses.h>
+
+// DAO header
+#include "daoShm.h" 
+
+typedef int bool_t;
+#ifndef TRUE
+#define TRUE	1
+#endif
+#ifndef FALSE
+#define FALSE	0
+#endif
+
+/*==========================================================================*/
+static int	sNdx=0;							/* board index */
+static int	sExit=0;						/* program exit code */
+
+//Need to install process with setuid.  Then, so you aren't running privileged all the time do this:
+uid_t euid_real;
+uid_t euid_called;
+uid_t suid;
+
+
+struct timespec tnow;
+double tlastupdatedouble;
+
+
+#define COMBINE_MAX 16
+
+IMAGE *shm;
+char shmName[32];
+int nbShm;
+// Max 16 different SHM to combine
+IMAGE *shmIn[COMBINE_MAX];
+
+float frequency;
+
+// Thread
+pthread_t controllerThread[COMBINE_MAX];
+int threadIdCtrl = 0;
+
+struct arg_struct {
+    int shmId;
+};
+
+// termination flag
+static int end     = 0;
+
+// termination function for SIGINT callback
+static void endme() 
+{
+    end = 1;
+}
+
+static char	*sArgv0=NULL;					/* name of executable */
+
+static void ShowHelp(void)
+{
+    daoInfo("%s of " __DATE__ " at " __TIME__ "\n",sArgv0);
+    daoInfo("   arguments:\n");
+    daoInfo("   -h               display this message and exit\n");
+    daoInfo("   -d               display program debug output\n");
+    daoInfo("   -l str           display str in output\n");
+    /*
+     **	Post init tests
+     */
+    daoInfo("   -L Nb            real time control loop: example daoShmCombiner -L shm N \n");
+    daoInfo("                    will combine shm0 + ... + shmN into shm\n");
+    /*
+     **	Timing tests
+     */
+    daoInfo("   -t nloops        test timing for i/o\n");
+    daoInfo("\n");
+}
+/*--------------------------------------------------------------------------*/
+void * shmNRealTimeLoop(void *thread_data)
+{
+    struct arg_struct *args = (struct arg_struct *)thread_data;
+    daoInfo("ThreadId=%d\n", args->shmId);
+
+    // MAIN LOOP
+    daoInfo("ENTERING LOOP\n");
+    fflush(stdout);
+    struct timespec t[2];
+    double elapsedTime;
+    clock_gettime(CLOCK_REALTIME, &t[1]);
+    float pauseTime;
+    pauseTime = 1e6/frequency-50;
+    int nbVal = shm[0].md[0].size[0] * shm[0].md[0].size[1];
+    // timing emulation there is a small offset of about 50 us...
+    // probalby due to the usleep function... not very accurate.
+    WINDOW * mainwin;
+    mainwin = initscr();
+    /*  Initialize ncurses  */
+    if ( mainwin  == NULL ) 
+    {
+	    daoError("Error initialising ncurses.\n");
+        return (void *)DAO_ERROR;
+    }
+    struct timespec timeout;
+    while (end==0) 
+    {
+        // Wait for SHM semaphore
+        clock_gettime(CLOCK_REALTIME, &timeout);
+        timeout.tv_sec +=1;
+        if (sem_timedwait(shmIn[0][args->shmId].semptr[0], &timeout) != -1)
+        {
+            if (daoShmCombineShm2Shm(shmIn, shm, nbShm, nbVal) == DAO_ERROR)
+            {
+                daoError("Combiner failed for thread %d\n", args->shmId);
+            }
+        }
+        // Update main SHM
+        usleep(pauseTime);
+        mvprintw(0, 0, "Shared Memory monitoring: %s/%s.im.shm\n", SHAREDMEMDIR, shm[0].md[0].name);
+        printw("-------------------------------------------------------------------\n"); 
+        printw("naxis       %d\n", shm[0].md[0].naxis); 
+        printw("size        %d, %d, %d\n", shm[0].md[0].size[0], shm[0].md[0].size[1], shm[0].md[0].size[2]); 
+        printw("nelement    %d\n", shm[0].md[0].nelement); 
+        printw("atype       %d\n", shm[0].md[0].atype); 
+        printw("cnt1        %d\n", shm[0].md[0].cnt1); 
+        printw("cnt2        %d\n", shm[0].md[0].cnt2); 
+        printw("timestamp   %ld\n", shm[0].md[0].atime.tsfixed.secondlong); 
+        printw("-------------------------------------------------------------------\n"); 
+        refresh();
+        t[0]=t[1];
+        clock_gettime(CLOCK_REALTIME, &t[1]);
+        elapsedTime = (t[1].tv_sec - t[0].tv_sec) * 1e3;    // sec to ms
+        elapsedTime += (t[1].tv_nsec - t[0].tv_nsec) / 1e6; // us to ms
+        elapsedTime = elapsedTime; // in sec... :-)
+    }
+    endwin();
+
+    daoInfo("EXITING MAIN LOOP\n");
+    fflush(stdout);
+
+    return DAO_SUCCESS;
+}
+    
+/*--------------------------------------------------------------------------*/
+static int prepRealTime()
+{
+    int status;
+    int k;
+    // register interrupt signal to terminate the main loop
+    signal(SIGINT, endme);
+
+    shm = (IMAGE*) malloc(sizeof(IMAGE));
+    daoShmShm2Img(shmName, "", &shm[0]);
+    char nameId[32];
+    // create shm (/tmp/<shmName><nameId>.im.shm)
+    for (k=0; k<nbShm; k++)
+    {
+        sprintf(nameId, "%02d", k);
+        shmIn[k] = (IMAGE *)malloc(sizeof(IMAGE));
+        daoShmShm2Img(nameId, shmName, &shmIn[k][0]);
+    }
+
+    clock_t launch, done;
+    double diff;
+    launch=clock();
+    status=1; 
+    usleep(1000);
+    done=clock();
+    diff = (double)(done - launch) / CLOCKS_PER_SEC;
+    daoInfo("\n%ld, %ld, %ld\n",done, launch, CLOCKS_PER_SEC);
+    daoInfo("SHM monitoring init status = %d, init time=%.3f\n", status, diff);
+    fflush(stdout);
+
+    int threadVal[nbShm];
+    struct arg_struct args[nbShm];
+    int threadCounter = 0;
+    for (threadCounter=0; threadCounter<nbShm; threadCounter++)
+    {
+        args[threadCounter].shmId = threadCounter;
+        threadVal[threadCounter] = pthread_create(&controllerThread[threadCounter],
+                                                     NULL,
+                                                     shmNRealTimeLoop,
+                                                     (void *)&args[threadCounter]);
+        if (threadVal[threadCounter] != 0)
+        {
+            daoError("Cannot create thread %d\n", threadCounter);
+            return DAO_ERROR;
+        }
+    }
+    // join last thread
+    pthread_join(controllerThread[threadCounter-1], NULL);
+
+    return DAO_SUCCESS;
+}
+
+static void DecodeArgs(int argc, char **argv)
+    /*
+     **	Parse the input arguments.
+     */
+{
+    char	*str;
+    int a1;
+
+    argv += 1;	argc -= 1;					/* skip program name */
+
+    while (argc-- > 0) {
+        daoDebug("DecodeArgs: working on '%s'/%d\n",*argv,argc);
+        str = *argv++;
+        if (str[0] != '-') {
+            daoError("Do not know arg '%s'\n",str);
+            ShowHelp();
+            exit(1);
+        }
+
+        switch (str[1]) {
+            case 'h':	ShowHelp(); exit(0);
+	    case 'd':	
+			(void)sscanf(*argv++,"%d",&daoLogLevel); argc -= 1;
+			break;
+            case 'l':
+                        daoInfo("%s\n",*argv);
+                        argv += 1; argc -= 1;
+                        break;
+
+            case 'b':	(void)sscanf(*argv++,"%d",&sNdx); argc -= 1;	break;
+            case 'u':
+                        (void)sscanf(*argv++,"%d",&a1); argc -= 1;
+                        daoDebug("will sleep for %d usec\n",a1);
+                        (void)usleep(a1);
+                        break;
+            case 'L':
+                        daoInfo("CAM real time control\n");
+                        (void)sscanf(*argv++,"%s", shmName);
+                        (void)sscanf(*argv++,"%d", &nbShm);
+                        daoInfo("shmName: %s \n", shmName);
+                        daoInfo("nbShm  : %d \n", nbShm);
+                        prepRealTime();
+                        break;
+            default:
+                        daoError("Do not know arg '%s'\n",str);
+                        ShowHelp();
+                        exit(2);
+        }
+    }
+
+    return;
+}
+/*==========================================================================*/
+int main(int argc, char **argv)
+    /*
+     **	Fetch the arguments and do what is requested
+     */
+{
+    int RT_priority = 93; //any number from 0-99
+    struct sched_param schedpar;
+
+    schedpar.sched_priority = RT_priority;
+    // r = seteuid(euid_called); //This goes up to maximum privileges
+    sched_setscheduler(0, SCHED_FIFO, &schedpar); //other option is SCHED_RR, might be faster
+    // r = seteuid(euid_real);//Go back to normal privileges
+
+    sArgv0 = *argv;
+
+    DecodeArgs(argc,argv);
+
+    return(sExit);
+}
+/*==========================================================================*/
+
+
+
