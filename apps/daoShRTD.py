e17ef15106cab9401e95631ec52c4480f12299c3
scetre <scetre@pm.me>
2023-02-03 09:24:04 +0000

    Created SH WFS simple python object

--- apps/daoShRTD.py
+++ apps/daoShRTD.py
@@ -8,8 +8,8 @@ import time
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    shmimName = '/tmp/image.im.sh'
-    shmcentName = '/tmp/centroids.im.sh'
+    shmimName = '/tmp/image.im.shm'
+    shmcentName = '/tmp/centroids.im.shm'
     try:
         opts, args = getopt.getopt(sys.argv[1:],"hi:c",["help", "shmimName=", "shmcentName"])
     except getopt.GetoptError:
@@ -24,15 +24,29 @@ if __name__ == '__main__':
         elif opt in ("-c", "--shmcentName"):
             shmcentName = str(arg)
     shmim = daoShm.shm(shmimName)
+    shmcent = daoShm.shm(shmcentName)
 
+    offset = 19
     plt.ion()
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    a=plt.imshow(shmim.get_data())
-    plt.colorbar()
-    plt.show()
+    fig, ax = plt.subplots()
+    iref=ax.imshow(shmim.get_data(), cmap='gray')
+    #cref=ax.plot(a.subaperture_center_coordinates[0].flatten()+offset,a.subaperture_center_coordinates[1].flatten()+offset,'+b')
+    cent = shmcent.get_data()
+    centh=ax.plot(cent[:,0]+offset, cent[:,1]+offset,'.r')
+    
+    # draw gridlines
+    ax.grid(which='major', axis='both', linestyle='-', color='k', linewidth=2)
+    ax.set_xticks(np.arange(offset-0.5, 450+offset-0.5+15, 15));
+    ax.set_yticks(np.arange(offset-0.5, 450+offset-0.5+15, 15));
+
+
+    #fig = plt.figure()
+    #ax = fig.add_subplot(111)
+    #a=plt.imshow(shmim.get_data())
+    #plt.colorbar()
+    #plt.show()
     while True:
-        a.set_data(shmim.get_data())
+        iref.set_data(shmim.get_data())
         fig.canvas.draw()
         fig.canvas.flush_events()
-        time.sleep(0.01)
+        time.sleep(0.1)
