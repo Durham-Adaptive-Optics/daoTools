36838f3c6a05653c75dd55fcedf3b0e61747b419
lazaraza <lazar.staykov@durham.ac.uk>
2023-02-09 15:37:28 +0000

    wip

--- apps/daoShRTD.py
+++ apps/daoShRTD.py
@@ -10,45 +10,42 @@ import matplotlib.pyplot as plt
 if __name__ == '__main__':
     shmimName = '/tmp/image.im.shm'
     shmcentName = '/tmp/centroids.im.shm'
+    shmrefName = '/tmp/references.im.shm'
     try:
-        opts, args = getopt.getopt(sys.argv[1:],"hi:c",["help", "shmimName=", "shmcentName"])
+        opts, args = getopt.getopt(sys.argv[1:],"hi:c:r",["help", "shmimName=", "shmcentName=", "shmrefName="])
     except getopt.GetoptError:
-      print('err, usage: doaShRTD.py -i <shmimName> -c <shmcentName>')
+      print('err, usage: doaShRTD.py -i <shmimName> -c <shmcentName> -r <referenceName>')
       sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print('daoShRTD.py -s <shmimName>')
+            print('daoShRTD.py -s <shmimName> -c<shmcentName> - r<shmrefName>')
             sys.exit()
         elif opt in ("-i", "--shmimName"):
             shmimName = str(arg)
         elif opt in ("-c", "--shmcentName"):
             shmcentName = str(arg)
+        elif opt in ("-c", "--shmrefName"):
+            shmrefName = str(arg)
     shmim = daoShm.shm(shmimName)
     shmcent = daoShm.shm(shmcentName)
+    shmref = daoShm.shm(shmrefName)
 
-    offset = 19
     plt.ion()
     fig, ax = plt.subplots()
     iref=ax.imshow(shmim.get_data(), cmap='gray')
-    #cref=ax.plot(a.subaperture_center_coordinates[0].flatten()+offset,a.subaperture_center_coordinates[1].flatten()+offset,'+b')
-    cent = shmcent.get_data()
-    centh=ax.plot(cent[:,0]+offset, cent[:,1]+offset,'.r')
+    cref=shmref.get_data()
+    crefh,=ax.plot(cref[:,0],cref[:,1],'+g')
+    #cent = shmcent.get_data()
+    cent=cref+shmcent.get_data();
+    centh,=ax.plot(cent[:,0], cent[:,1],'.r')
     
-    # draw gridlines
-    ax.grid(which='major', axis='both', linestyle='-', color='k', linewidth=2)
-    ax.set_xticks(np.arange(offset-0.5, 450+offset-0.5+15, 15));
-    ax.set_yticks(np.arange(offset-0.5, 450+offset-0.5+15, 15));
-
-    #fig = plt.figure()
-    #ax = fig.add_subplot(111)
-    #a=plt.imshow(shmim.get_data())
-    #plt.colorbar()
-    #plt.show()
     while True:
+        cref=shmref.get_data()
+        cent=cref+shmcent.get_data();
         iref.set_data(shmim.get_data())
-        cent = shmcent.get_data()
-        iref.set_data(shmim.get_data())
-        centh.set_data(cent[:,0]+offset, cent[:,1]+offset)
+        crefh.set_data(cref[:,0], cref[:,1])
+        centh.set_data(cent[:,0], cent[:,1])
         fig.canvas.draw()
         fig.canvas.flush_events()
         time.sleep(0.1)
+    
