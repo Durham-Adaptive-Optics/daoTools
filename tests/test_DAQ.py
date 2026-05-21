f69a0ce43d632a22c90c9555b2d2b38ea5ae21a3
Thomas Davies <thomasdavies1001@gmail.com>
2026-05-21 14:04:16 +0100

    debug CI tests.

--- tests/test_DAQ.py
+++ tests/test_DAQ.py
@@ -157,7 +157,7 @@ def smem_tester_(tmp_directory, client, tool_inst, shape, dtype, rollover: bool,
         sample = new_sample()
         smem.set_data(sample)
         sample_history_add()
-        time.sleep(1) # feed sample in slowly as we want to ensure correctness, not test performance.
+        time.sleep(10) # feed sample in slowly as we want to ensure correctness, not test performance.
     
     # Await record to finish
     client.daq_session_await_finish(timeout=1)
